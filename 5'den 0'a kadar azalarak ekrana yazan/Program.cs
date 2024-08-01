1c608e04a1b086fe9e6c712ded3fb781a8aada91
Serap BALCI <81075168+reset-34@users.noreply.github.com>
2024-08-01 16:00:41 +0300

    Add project files.

---
+++ 5'den 0'a kadar azalarak ekrana yazan/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5_den_0_a_kadar_azalarak_ekrana_yazan
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i =5; i>0;i--)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.ReadLine();
+        }
+
+    }
+}
